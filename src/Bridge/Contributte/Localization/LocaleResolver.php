9b2f051045a9d6b4d24f713fd19e68ed595e7359
tg666 <tomasglawaty@icloud.com>
2020-11-19 08:10:57 +0100

    Fix: Changed implementation of `LocaleResolver` class in Contributte Bridge (method `::addResolver()` can not be overloaded because a return type is declared as `self`, this causes warning)

--- src/Bridge/Contributte/Localization/LocaleResolver.php
+++ src/Bridge/Contributte/Localization/LocaleResolver.php
@@ -5,42 +5,20 @@ declare(strict_types=1);
 namespace SixtyEightPublishers\TranslationBridge\Bridge\Contributte\Localization;
 
 use Contributte\Translation\Translator;
-use Contributte\Translation\LocalesResolvers\ResolverInterface;
 use Contributte\Translation\LocaleResolver as ContributteLocaleResolver;
 use SixtyEightPublishers\TranslationBridge\Localization\TranslatorLocaleResolverInterface;
 
 final class LocaleResolver extends ContributteLocaleResolver
 {
-	/** @var \Contributte\Translation\LocaleResolver  */
-	private $inner;
-
 	/** @var \SixtyEightPublishers\TranslationBridge\Localization\TranslatorLocaleResolverInterface  */
 	private $translatorLocaleResolver;
 
 	/**
-	 * @param \Contributte\Translation\LocaleResolver                                                $inner
 	 * @param \SixtyEightPublishers\TranslationBridge\Localization\TranslatorLocaleResolverInterface $translatorLocaleResolver
 	 */
-	public function __construct(ContributteLocaleResolver $inner, TranslatorLocaleResolverInterface $translatorLocaleResolver)
+	public function __construct(TranslatorLocaleResolverInterface $translatorLocaleResolver)
 	{
-		$this->inner = $inner;
 		$this->translatorLocaleResolver = $translatorLocaleResolver;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getResolvers(): array
-	{
-		return $this->inner->getResolvers();
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function addResolver(ResolverInterface $resolver): ContributteLocaleResolver
-	{
-		return $this->inner->addResolver($resolver);
 	}
 
 	/**
